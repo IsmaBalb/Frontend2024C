b7d1213530e0752e8f8138d63fa36fcd3cf15880
Ismael Florencio Balbuena <isc20350270@gmail.com>
2024-02-20 17:37:10 -0600

    Actualizado

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,10 +1,47 @@
+import { useState } from 'react'
 import './App.css'
 const numericButtonsClasses = 'btn btn-outline-info w-100'
 const operatorButtonsClasses = 'btn btn-outline-warning w-100'
 const specialButtonsClasses = 'btn btn-outline-danger'
 
 function App(){
-  return(
+    const [display, setDisplay] = useState({value: '0', hasPoint: false,})
+
+    const updateDisplay = (value) =>{
+     if (value === '.'){
+       if(display.hasPoint){
+         return 
+       }
+       setDisplay({
+         ...display,
+         value: display.value + value,
+         hasPoint: true,
+       })
+       return 
+     }
+     if(display.value === '0'){
+       setDisplay({
+         ...display,
+         value: value,
+       })
+       retrun
+     }
+     setDisplay({
+       ...display,
+       value: display.value + value,
+       
+     })
+      
+    }
+   const clearDisplay =() =>{
+     setDisplay({
+     ...display,
+     value: '0',
+     hasPoint: false,
+   })
+   }
+   
+    return(
     <div>
       <h1>Calculator</h1>
       <hr />
@@ -12,13 +49,13 @@ function App(){
         <tbody>
          <tr>
         <td className='text-end' colSpan={4}>
-          <h2>0</h2>
+          <h2>{display.value}</h2>
         </td>
            </tr>
            <tr>
             <td>
               <button
-                className={operatorButtonsClasses} type= 'button'>C</button>  
+                className={operatorButtonsClasses} type= 'button' onClick={clearDisplay}>C</button>  
             </td>
             <td>
             <button
@@ -36,15 +73,15 @@ function App(){
            <tr>
             <td>
             <button
-                className={operatorButtonsClasses} type= 'button'>7</button>
+                className={numericButtonsClasses} type= 'button' onClick={()=>updateDisplay('7')}>7</button>
             </td>
             <td>
             <button
-                className={operatorButtonsClasses} type= 'button'>8</button>
+                className={numericButtonsClasses} type= 'button' onClick={()=>updateDisplay('8')}>8</button>
             </td>
             <td>
             <button
-                className={operatorButtonsClasses} type= 'button'>9</button>
+                className={numericButtonsClasses} type= 'button' onClick={()=>updateDisplay('9')}>9</button>
             </td>
             <button
                 className={operatorButtonsClasses} type= 'button'>X</button>
@@ -52,15 +89,15 @@ function App(){
            <tr>
             <td>
             <button
-                className={numericButtonsClasses} type= 'button'>4</button>
+                className={numericButtonsClasses} type= 'button' onClick={()=>updateDisplay('4')}>4</button>
             </td>
             <td>
             <button
-                className={numericButtonsClasses} type= 'button'>5</button>
+                className={numericButtonsClasses} type= 'button' onClick={()=>updateDisplay('5')}>5</button>
             </td>
             <td>
             <button
-                className={numericButtonsClasses} type= 'button'>6</button>
+                className={numericButtonsClasses} type= 'button' onClick={()=>updateDisplay('6')}>6</button>
             </td>
             <td>
             <button
@@ -70,15 +107,15 @@ function App(){
            <tr>
            <td>
            <button
-                className={numericButtonsClasses} type= 'button'>1</button>
+                className={numericButtonsClasses} type= 'button' onClick={()=>updateDisplay('1')}>1</button>
             </td>
             <td>
             <button
-                className={numericButtonsClasses} type= 'button'>2</button>
+                className={numericButtonsClasses} type= 'button' onClick={()=>updateDisplay('2')}>2</button>
             </td>
             <td>
             <button
-                className={numericButtonsClasses} type= 'button'>3</button>
+                className={numericButtonsClasses} type= 'button' onClick={()=>updateDisplay('3')}>3</button>
             </td>
             <td>
             <button
@@ -88,11 +125,11 @@ function App(){
            <tr>
            <td colSpan={2}>
            <button
-                className={numericButtonsClasses} type= 'button'>0</button>
+                className={numericButtonsClasses} type= 'button' onClick={()=>updateDisplay('0')}>0</button>
             </td>
             <td>
             <button
-                className={numericButtonsClasses} type= 'button'>.</button>
+                className={numericButtonsClasses} type= 'button' onClick={()=>updateDisplay('.')}>.</button>
             </td>
             <td>
             <button
