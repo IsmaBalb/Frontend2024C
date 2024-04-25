e616944adf7c975a52aeb7d5ff3bb0c2b03b2b9f
Ismael Florencio Balbuena <isc20350270@gmail.com>
2024-04-25 10:47:29 -0600

    Actualizado

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,4 +1,5 @@
 import List from "./List" 
+import TaskModal from "./TaskModal"
 const taskList = [ 
   { 
     id: 1, 
@@ -27,22 +28,29 @@ const taskList = [
  
 ] 
  
-function App() { 
+function App() {
+
+  return (
+    <div className="container">
+      <h1>To Do List</h1>
+      <hr />
+      <List 
+      taskList={taskList}
+      />
+
+<hr />
+    <div className="text-end">
+      <TaskModal />
+      <button type="button" className="btn btn-primary" 
+      data-bs-toggle="modal"
+      data-bs-target="#TaskModal">
+        <i className="bi bi-plus-lg"></i> Add </button>
   
-  return ( 
-     
-      <div className="container"> 
-          <h1>To Do List</h1> 
-          <hr /> 
-          <List  
-      taskList={taskList} 
-      /> 
-      <hr /> 
-      <div className="text-end"> 
-        <button type="button" className="btn btn-outline-primary"><i className="bi bi-plus-lg"></i>Add</button> 
-      </div> 
-    </div> 
-  ) 
-} 
+    </div>
+    </div>
+   
+  )
+}
+
  
 export default App
